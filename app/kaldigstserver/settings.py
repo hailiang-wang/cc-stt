aa4d101b5a483abb98593a77f2fc5bf727473850
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2018-09-17 13:45:06 +0800

    init

---
+++ app/kaldigstserver/settings.py
@@ -0,0 +1,11 @@
+'''
+Copyright (c) 2014, alumae
+All rights reserved.
+Created on Jun 7, 2013
+
+@author: tanel
+'''
+
+from tornado.options import define
+
+define("port", default=8888, help="run on the given port", type=int)
